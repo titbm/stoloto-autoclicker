10f303f2edecad268f6dffb629b07bfb2fda7a6f
titbm <titbmm@gmail.com>
2025-05-12 21:55:52 +0300

    Улучшение: Умное поведение иконки расширения
    - Если активна рабочая страница, по клику открывается окно поиска
    - В противном случае открывается новая вкладка с рабочей страницей
    - Упрощен манифест, удален лишний фоновый скрипт

---
+++ event_page.js
@@ -0,0 +1,18 @@
+chrome.action.onClicked.addListener(async (tab) => {
+    // URL рабочей страницы приложения
+    const workPageUrl = 'https://www.stoloto.ru/ruslotto/game?viewType=favorite';
+    
+    // Если текущая вкладка - рабочая страница приложения
+    if (tab.url === workPageUrl) {
+        // Открываем popup
+        await chrome.action.setPopup({ popup: 'popup.html' });
+        // Имитируем повторный клик, чтобы открыть popup
+        await chrome.action.openPopup();
+    } else {
+        // Если текущая вкладка не рабочая, открываем новую
+        await chrome.tabs.create({ url: workPageUrl });
+    }
+    
+    // Сбрасываем popup, чтобы следующий клик снова прошёл через этот обработчик
+    await chrome.action.setPopup({ popup: '' });
+});
